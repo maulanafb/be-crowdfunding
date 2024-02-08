4c853cb2efb106b3ddf47fd38d08e184988e5b13
unknown <gagas112212@gmail.com>
2024-02-08 08:12:15 +0700

    feat:campaign

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,24 +1,38 @@
 package handler
 
+import (
+	"be_crowdfunding/campaign"
+	"be_crowdfunding/helper"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
 // tangkap parameter di handler
 // handler ke service
 // di service dia yang menentukan method repository mana yang di call
 // repository : GetAll , Get By User ID
 // db
 
-import "time"
+type campaignHandler struct {
+	service campaign.Service
+}
 
-type Campaign struct {
-	ID               int
-	UserID           int
-	Name             string
-	ShortDescription string
-	Description      string
-	Perks            string
-	BackerCount      int
-	GoalAmount       int
-	CurrentAmount    int
-	Slug             string
-	CreatedAt        time.Time
-	UpdatedAt        time.Time
+func NewCampaignHandler(service campaign.Service) *campaignHandler {
+	return &campaignHandler{service}
+}
+
+// api/v1/campaigns
+
+func (h *campaignHandler) GetCampaigns(c *gin.Context) {
+	userID, _ := strconv.Atoi(c.Query("user_id"))
+	campaigns, err := h.service.GetCampaigns(userID)
+	if err != nil {
+		response := helper.APIResponse("Error to get campaigns", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	response := helper.APIResponse("List of campaigns", http.StatusOK, "success", campaigns)
+	c.JSON(http.StatusOK, response)
 }
