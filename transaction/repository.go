e2e72f7452d026eb51ae0b3da39496e65b60e771
unknown <gagas112212@gmail.com>
2024-02-11 20:49:02 +0700

    init midtrans

--- transaction/repository.go
+++ transaction/repository.go
@@ -9,6 +9,7 @@ type repository struct {
 type Repository interface {
 	GetCampaignID(campaignID int) ([]Transaction, error)
 	GetByUserID(userID int) ([]Transaction, error)
+	Save(transaction Transaction) (Transaction, error)
 }
 
 func NewRepository(db *gorm.DB) *repository {
@@ -31,4 +32,12 @@ func (r *repository) GetByUserID(userID int) ([]Transaction, error) {
 		return transactions, err
 	}
 	return transactions, nil
+}
+
+func (r *repository) Save(transaction Transaction) (Transaction, error) {
+	err := r.db.Create(&transaction).Error
+	if err != nil {
+		return transaction, err
+	}
+	return transaction, nil
 }
